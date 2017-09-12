3dcdae3f3ab69ffd4699c7e19334a3572a117b12
AAKASH HANDA <AAKASH HANDA>
2017-09-12 16:33:28 +0530

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,19 @@ import {Employee} from '../models/employee.model';
 })
 export class HomeComponent {
   languages =['English','Spanish','Other'];
-  model=new Employee('Bhumika', 'Bajpai',true,"male","English");
-
-  firstNameToUpperCase(value: string){
+  model=new Employee('Bhumika', 'Bajpai',9643018019,true,"male","default");
+  hasPrimaryLanguageError=false;
+  validatePrimaryLanguage(event){
+    console.log('lang: '+this.model.primaryLanguage)
+    if(this.model.primaryLanguage === 'default')
+      this.hasPrimaryLanguageError = true;
+    else
+      this.hasPrimaryLanguageError = false;
+  }
+  /*firstNameToUpperCase(value: string){
     if(value.length > 0)
       this.model.firstName= value.charAt(0).toUpperCase()+ value.slice(1);
     else
       this.model.firstName = value;
-  }
+  }*/
 }
--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -2,6 +2,7 @@ export class Employee {
     constructor(
         public firstName:string,
         public lastName:string,
+        public phoneNumber:number,
         public isFullTime:boolean,
         public gender:string,
         public primaryLanguage:string
