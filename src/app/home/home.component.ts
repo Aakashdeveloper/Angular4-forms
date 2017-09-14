af4cec58d1fcef0539393af7beea3da8bec4d0d4
AAKASH HANDA <AAKASH HANDA>
2017-09-14 19:12:13 +0530

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Employee} from '../models/employee.model';
+import {FormPoster} from '../services/form-poster.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -10,6 +12,23 @@ export class HomeComponent {
   languages =['English','Spanish','Other'];
   model=new Employee('Bhumika', 'Bajpai',9643018019,true,"male","default");
   hasPrimaryLanguageError=false;
+
+  constructor(private formPoster: FormPoster){
+
+  }
+
+  submitForm(form : NgForm){
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    if(this.hasPrimaryLanguageError)
+      return;
+    this.formPoster.postEmployeeForm(this.model)
+      .subscribe(
+        data=> console.log('success:', data),
+        err => console.log('error: ',err)
+      )
+    console.log(this.model);
+  }
+
   validatePrimaryLanguage(event){
     console.log('lang: '+this.model.primaryLanguage)
     if(this.model.primaryLanguage === 'default')
